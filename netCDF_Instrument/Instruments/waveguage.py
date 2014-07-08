85c68b03fcf76a359dc421cc01648528c07d9477
shape87 <gpetrochenkov@gmail.com>
2014-07-08 09:31:37 -0400

    Abstracted Tests

--- netCDF_Instrument/Instruments/waveguage.py
+++ netCDF_Instrument/Instruments/waveguage.py
@@ -1,11 +1,12 @@
 from Instruments.sensor import Sensor
+from Instruments.InstrumentTests import PressureTests
 import numpy as np
 from datetime import datetime
 from pytz import timezone
 import pandas as pd
 import pytz
 
-class Waveguage(Sensor):
+class Waveguage(Sensor, PressureTests):
     """Reads in an ASCII file output by a Waveguage pressure sensor
     from Ocean Sensor Systems Inc.
 
@@ -16,13 +17,6 @@ class Waveguage(Sensor):
     def __init__(self):
         self.tz_info = pytz.timezone("US/Eastern")
         super(Waveguage, self).__init__()
-         
-        self.local_frequency_range = [11, 19] # for IOOS test 17
-        self.mfg_frequency_range = [10, 20] # for IOOS test 17
-        self.max_rate_of_change = 20
-        self.prev_value = True # for IOOS test 20
-        self.five_count_list = list()
-        
        
     def read(self):
         """Sets start_time to a datetime object, utc_millisecond_data
@@ -83,59 +77,6 @@ class Waveguage(Sensor):
         data_array = np.array(data.p, dtype='float64')
         return data_array
     
-    def test_16_stucksensor(self):
-        self.pressure_test16_data = [self.get_16_value(x) for x in self.pressure_data]
-        
-    def test_17_frequencyrange(self):
-        self.pressure_test17_data = [self.get_17_value(x) for x in self.pressure_data]
-        
-    def test_20_rateofchange(self):
-        self.pressure_test20_data = [self.get_20_value(x) for x in self.pressure_data]
-    
-    def get_15_value(self):
-        print('start mean')
-        print('mean', np.mean(self.pressure_data))
-        print('mean', np.mean(self.pressure_data))
-        
-               
-    def get_16_value(self,x):
-           
-           
-       
-        if len(self.five_count_list) > 5:
-            self.five_count_list.pop()
-            
-        flags = np.count_nonzero(np.equal(x,self.five_count_list))
-        self.five_count_list.insert(0,x)
-        
-        if flags <= 2:
-            return 1
-        elif flags <= 4:
-            return 3
-        else:
-            return 4  
-            
-    def get_17_value(self, x):
-        print(self.local_frequency_range[0])
-        if np.greater_equal(x,self.local_frequency_range[0]) and \
-        np.less_equal(x,self.local_frequency_range[1]):
-            return 1
-        elif np.greater_equal(x,self.mfg_frequency_range[0]) and \
-                            np.less_equal(x,self.mfg_frequency_range[1]):
-            return 3
-        else:
-            return 4
-        
-    def get_20_value(self, x):
-      
-        if np.isnan(self.prev_value) or \
-        np.less_equal(np.abs(np.subtract(x,self.prev_value)), self.max_rate_of_change):
-            self.prev_value = x
-            return 1
-        else:
-            self.prev_value = x
-            return 4
-
 if __name__ == '__main__':
     # Just for testing!
     wg = Waveguage()
