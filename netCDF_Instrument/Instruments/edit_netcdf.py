48dc17e58e1cc773f91b0270e7d48b36f9be8c0a
shape87 <gpetrochenkov@gmail.com>
2014-07-15 09:42:26 -0400

    changes

--- netCDF_Instrument/Instruments/edit_netcdf.py
+++ netCDF_Instrument/Instruments/edit_netcdf.py
@@ -15,28 +15,35 @@ class NetCDFReader(object):
     def __init__(self):
         self.in_file_name = os.path.join("benchmark","WaveLog.nc")
         self.pressure_data = None
+        self.series = None
+        self.dataframe_vals = dict()
+        self.dataframe = None
         
         
-    def read_file(self):
-        nc = netCDF4.Dataset(self.in_file_name)
-#         for p in nc.variables.keys():
-#             print(p)
+    def read_file(self,x):
+        nc = netCDF4.Dataset(x)
+        
         pressure = nc.variables['sea_water_pressure']
         times = nc.variables['time']
-        time_convert = netCDF4.num2date(times[:],'milliseconds since 1970-01-01 00:00:00')
+        #this method converts the milliseconds in to date times
+        time_convert = netCDF4.num2date(times[:],times.units)
         
-        if str(nc.variables.keys).find('temperature_at_transducer') != -1:
-            print('found')
-            temperature = nc.variables['temperature_at_transducer']
-            data = {'pressure': pd.Series(pressure,index=time_convert), \
-                    'temperature': pd.Series(temperature,index=time_convert)}
-            self.pressure_data = pd.DataFrame(data)
-        else:
-            self.pressure_data = pd.Series(pressure,index=time_convert)
+#         if str(nc.variables.keys).find('temperature_at_transducer') != -1:
+#             print('found')
+#             temperature = nc.variables['temperature_at_transducer']
+#             data = {'pressure': pd.Series(pressure,index=time_convert), \
+#                     'temperature': pd.Series(temperature,index=time_convert)}
+#             return pd.DataFrame(data)
+#         else:
+        return pd.Series(pressure,index=time_convert)
         
-        
-       
-        
+    def get_series(self, filename):
+        return self.read_file(filename)
+    
+    def get_dataframe(self):
+        for x in self.file_names:
+            self.dataframe_vals[x] = self.read_file(x)
+        self.dataframe = pd.DataFrame(self.dataframe_vals)
         
 if __name__ == "__main__":
     
--- netCDF_Instrument/Instruments/grapher.py
+++ netCDF_Instrument/Instruments/grapher.py
@@ -8,53 +8,83 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 import numpy as np
+import datetime
+from Instruments.edit_netcdf import NetCDFReader
 
-class Grapher(object):
+class Grapher(NetCDFReader):
     
     def __init__(self):
+        super().__init__()
         self.pressure_data = None
-        self.in_file_name = os.path.join("benchmark","WaveLog.nc")
-
-    def retrieve_data(self):
-        nc = netCDF4.Dataset(self.in_file_name)
-        pressure = nc.variables['sea_water_pressure']
-        times = nc.variables['time']
+        self.file_names = [os.path.join("benchmark","RBRvirtuoso.nc"),os.path.join("benchmark","RBRsolo2.nc"),os.path.join("benchmark","RBRsolo1.nc"),
+                           os.path.join("benchmark","Neag2-1.nc")]
+        self.colors = ["yellow", "orange", "purple", "green","blue", "red"]
+        self.names = []
+        self.plot_data = []
+        self.mean_list = []
+        self.ymax = 0
+        self.ymin = 10000000
         
-        time_convert = netCDF4.num2date(times[:],'milliseconds since 1970-01-01 00:00:00')
-#         if nc.variables['temperature'] != None:
-#             temperature = nc.variables['temperature']
-#             self.pressure_data = pd.Series(pressure,temperature,index=time_convert)
-#         else:
-        self.pressure_data = pd.Series(pressure,index=time_convert)
+    def plot_part(self, series, name):
+        self.names.append(name)
+        self.mean_list.append(np.mean(series))
+        skipIndex = 1
+        if len(series) > 10000:
+            skipIndex = int(len(series) / 10000)
+            if skipIndex < 100:
+                skipIndex = 100
+        
+        max = np.max(series[::skipIndex])   
+        min = np.min(series[::skipIndex])
+        
+        if max > self.ymax:
+            self.ymax = max
+        if min < self.ymin:
+            self.ymin = min
+            
+        if name == "benchmark\RBRsolo1.nc":
+            times = [x + datetime.timedelta(hours=1) for x in series.index[::skipIndex]]
+            print(times[0])
+            return plt.plot(times,series[::skipIndex], \
+                  color=self.colors.pop(), linewidth="3.0", linestyle="-")
+            
+            
+        return plt.plot(series.index[::skipIndex],series[::skipIndex], \
+                  color=self.colors.pop(), linewidth="3.0", linestyle="-")
         
     def plot_graph(self):
         fig = plt.figure(figsize=(12,4))
         ax = fig.add_subplot(111)
-       
-        
-        skipIndex = 1
-        if len(self.pressure_data) > 10000:
-            skipIndex = int(len(self.pressure_data) / 10000)
-            if skipIndex < 100:
-                skipIndex = 100
-             
-        #self.pressure_data[::100].plot(ax=ax,title='Pressure Data')
         ax.set_ylabel('Pressure in dbars')
-        p1,= plt.plot(self.pressure_data.index[::skipIndex],self.pressure_data[::skipIndex], \
-                  color="blue", linewidth="1.0", linestyle="-")
+        plt.grid(b=True, which='major', color='grey', linestyle="-")
+        time = None
+        for x in self.dataframe:     
+        #self.pressure_data[::100].plot(ax=ax,title='Pressure Data')
+            p1, = self.plot_part(self.dataframe[x], x)
+            self.plot_data.append(p1)
+            time = self.dataframe[x].index
         
        
-        
-        mean = np.mean(self.pressure_data)
-        p2, = plt.plot(self.pressure_data.index,np.repeat(mean, len(self.pressure_data)),color="orange", \
+        mean = np.mean(self.mean_list)
+        p2, = plt.plot(time,np.repeat(mean, len(time)),color="orange", \
                  linewidth="2.0", linestyle="-")
+        self.plot_data.append(p2)
         plt.title("Pressure Data")
-        plt.legend([p1, p2], ["Pressure", "Mean"])
+        self.names.append("Mean")
+        self.names.append("Max/Min")
+        p3, = plt.plot(time,np.repeat(self.ymax, len(time)), color="orange", \
+                linewidth="1.0", linestyle="--")
+        self.plot_data.append(p3)
+        p4, = plt.plot(time,np.repeat(self.ymin, len(time)), color="orange", \
+                linewidth="1.0", linestyle="--")
+        plt.legend(self.plot_data, self.names)
+        plt.xlim(time[0]  + datetime.timedelta(minutes=25),time[::-1][0] + datetime.timedelta(minutes=5))
+        
         plt.show()
         
 if __name__ == "__main__":
     
     #--create an instance    
     graph = Grapher()
-    graph.retrieve_data()
+    graph.get_dataframe()
     graph.plot_graph()
