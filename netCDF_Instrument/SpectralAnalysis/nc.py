d7021d6ad74c8dffbda49552d7270cb02744d2d3
Chris Mazzullo <chris.mazzullo@gmail.com>
2014-08-08 11:55:15 -0400

    Refactored nc.py

--- netCDF_Instrument/SpectralAnalysis/nc.py
+++ netCDF_Instrument/SpectralAnalysis/nc.py
@@ -1,66 +1,61 @@
+"""
+A few convenience methods for quickly extracting/changing data in netCDFs
+"""
+
 import netCDF4
 import os
 
-
+# Append new variables
 
 def append_air_pressure(fname, p, station, lat, lon):
-    nc = netCDF4.Dataset(fname, 'a', format='NETCDF4_CLASSIC')
-    try:
-        pvar = nc.createVariable('air_pressure', 'f8', ('time',))
-    except RuntimeError:
-        print('Output file already contains air pressure!')
-        return False
-    pvar.ioos_category = 'Pressure'
-    pvar.comment = ('This is air pressure pulled from the NOAA '
+    name = 'air_pressure'
+    comment = ('This is air pressure pulled from the NOAA '
                     'database of buoy barometric pressure readings. '
                     'You can find it at '
                     '"http://opendap.co-ops.nos.noaa.gov/axis/". The '
                     'buoy used corresponds to the station number '
                     'given in the "station" attribute of this '
                     'variable.')
-    pvar.standard_name = 'air_pressure'
-    pvar.max = 1000
-    pvar.min = -1000
-    pvar.short_name = 'air_pressure'
-    pvar.ancillary_variables = ''
-    pvar.add_offset = 0.0
-    pvar.coordinates = 'time latitude longitude altitude'
-    pvar.long_name = 'buoy pressure record'
-    pvar.nodc_name = 'PRESSURE'
-    pvar.scale_factor = 1.0
-    pvar.units = 'decibar'
-    pvar.compression = 'not used at this time'
-    pvar[:] = p
-    return True
+    long_name = 'buoy pressure record'
+    append_variable(fname, name, p, comment, standard_name=name, 
+                    short_name=name, long_name=long_name, station=station,
+                    lat=lat, lon=lon, buoy=True)
 
 
 def append_corrected_water_pressure(fname, p):
-    nc = netCDF4.Dataset(fname, 'a', format='NETCDF4_CLASSIC')
+    comment = ('The corrected water pressure is the "sea_water_pressure" '
+               'variable minus the "air_pressure" variable.')
     name = 'sea_water_pressure_due_to_sea_water'
-    try:
-        pvar = nc.createVariable(name, 'f8', ('time',))
-    except RuntimeError:
-        print('Output file already contains corrected pressure!')
-        return
+    append_variable(fname, name, p, comment, standard_name=name, 
+                    short_name='corrected_pressure', long_name=name)
+
+
+def append_variable(fname, name, p, comment='', standard_name='', 
+                    short_name='', long_name='', station=None, lat=None, 
+                    lon=None, buoy=False):
+    nc = netCDF4.Dataset(fname, 'a', format='NETCDF4_CLASSIC')
+    pvar = nc.createVariable(name, 'f8', ('time',))
     pvar.ioos_category = 'Pressure'
-    pvar.comment = ('The corrected water pressure is the '
-                    '"sea_water_pressure" variable minus the '
-                    '"air_pressure" variable.')
-    pvar.standard_name = name
+    pvar.comment = comment
+    pvar.standard_name = standard_name
     pvar.max = 1000
     pvar.min = -1000
-    pvar.short_name = 'corrected_pressure'
+    pvar.short_name = short_name
     pvar.ancillary_variables = ''
     pvar.add_offset = 0.0
     pvar.coordinates = 'time latitude longitude altitude'
-    pvar.long_name = 'corrected pressure record'
+    pvar.long_name = long_name
     pvar.nodc_name = 'PRESSURE'
     pvar.scale_factor = 1.0
     pvar.units = 'decibar'
     pvar.compression = 'not used at this time'
+    if buoy:
+        pvar.station = station
+        pvar.station_latitude = lat
+        pvar.station_longitude = lon
     pvar[:] = p
-    return True
 
+# Get variable data
 
 def get_time(fname):
     return get_variable_data(fname, 'time')
@@ -86,6 +81,8 @@ def get_variable_data(fname, variable_name):
     return v
 
 
+# Get global data
+
 def get_initial_pressure(fname):
     return get_global_attribute(fname, 'initial_pressure')
 
@@ -101,32 +98,3 @@ def get_device_depth(fname):
 def get_global_attribute(fname, name):
     nc = netCDF4.Dataset(fname)
     return getattr(nc, name)
-    
-def write(fname, in_t, in_p):
-    remove_if_existing(fname)
-    ds = netCDF4.Dataset(fname, 'w', format="NETCDF4_CLASSIC")
-    ds.createDimension("time", len(in_t))
-    t = ds.createVariable('time', 'f8', ('time',))
-    p = ds.createVariable('sea_water_pressure', 'f8', ('time',))
-    t[:] = in_t
-    p[:] = in_p
-
-
-def remove_if_existing(fname):
-    if os.path.isfile(fname):
-        os.remove(fname)
-
-
-def add_height_to_file(fname, height):
-    writer = netCDF4.Dataset(fname, 'a', format='NETCDF4_CLASSIC')
-    wave_height = writer.createVariable('wave_height','f8',('time',))
-    wave_height.units = 'meters'
-    wave_height[:] = height
-    writer.close()
-
-
-if __name__ == '__main__':
-    fname = ('C:\\Users\\cmazzullo\\wave-sensor-test-data\\'
-             'wave-guage-14-07-2014.csv.nc')
-    print(fname)
-    nc = netCDF4.Dataset(fname)
