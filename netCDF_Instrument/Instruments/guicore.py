4d16b56eba2219c5b08c3c7a002dd542232bfd5a
Chris Mazzullo <chris.mazzullo@gmail.com>
2014-08-20 11:49:27 -0400

    Documented guicore

--- netCDF_Instrument/Instruments/guicore.py
+++ netCDF_Instrument/Instruments/guicore.py
@@ -1,5 +1,11 @@
+"""
+Contains a gui that interfaces with this package's netCDF-writing
+modules. Also contains some convenience methods for other guis and a
+general class, MessageDialog, for creating custom dialog boxes.
+"""
+
 from functools import partial
-from tkinter import *
+import tkinter
 from tkinter import ttk
 from tkinter.filedialog import askopenfilename as fileprompt
 from pytz import timezone
@@ -14,20 +20,26 @@ from Instruments.measuresys import MeasureSysLogger
 from Instruments.hobo import Hobo
 
 
+__instruments__ = {'LevelTroll': Leveltroll,
+                   'RBRSolo': RBRSolo,
+                   'Wave Guage': Waveguage,
+                   'USGS Homebrew': House,
+                   'Measurement Specialties': MeasureSysLogger,
+                   'HOBO': Hobo}
+
 class Wavegui:
+    """ GUI for csv-to-netCDF conversion. """
+    def __init__(self, parent, air_pressure=False):
+        self.root = parent
+        parent.title("USGS Wave Data")
 
+        if air_pressure:
+            self.df_history = 'gui_df_history.txt'
+        else:
+            self.df_history = 'gui_df_history_air.txt'
 
-    def __init__(self, root, air_pressure=False):
-        self.instruments = {'LevelTroll': Leveltroll,
-                            'RBRSolo': RBRSolo,
-                            'Wave Guage': Waveguage,
-                            'USGS Homebrew': House,
-                            'Measurement Specialties': MeasureSysLogger,
-                            'HOBO': Hobo}
-        self.root = root
-        self.df_history = 'gui_df_history.txt'
         self.global_history = 'gui_global_history.txt'
-        self.root.title("USGS Wave Data")
+
         self.air_pressure = air_pressure
         self.global_fields = [
             Variable(label='Your full name:', autosave=True,
@@ -43,75 +55,84 @@ class Wavegui:
 
 
     def initialize(self):
+        """
+        Make the initial frames and entry fields.
+
+        If no files are selected, just creates a frame where a user
+        can input global variables. If some files are selected,
+        creates both a frame for global variables and a frame for
+        file-specific variables.
+        """
         try:
             self.global_frame.destroy()
             self.file_frame.destroy()
         except AttributeError:
             pass
+
         self.global_frame = make_frame(self.root)
         add_label(self.global_frame, 'Global Settings:')
         entries = make_frame(self.global_frame)
         for row, var in enumerate(self.global_fields):
             self.make_widget(entries, var, row)
-        entries.pack(fill=BOTH, expand=1)
-        def save_globals():
-            self.save_history(self.global_history, self.global_fields)
-        def load_globals():
-            self.load_history(self.global_history, self.global_fields)
+        entries.pack(fill='both', expand=1)
+        save_globals = partial(self.save_history, self.global_history,
+                               self.global_fields)
+        load_globals = partial(self.load_history, self.global_history,
+                               self.global_fields)
         buttons = [("Add File(s)", self.add_files, 0, 0),
                    ("Save Globals", save_globals, 0, 1),
                    ("Load Globals", load_globals, 0, 2),
                    ("Quit", self.root.destroy, 0, 5)]
         if self.filenames:
-            def load_per_file():
-                for df in self.datafiles:
-                    self.load_history(self.df_history, df.fields)
-            buttons += [("Load Default", load_per_file, 0, 3),
+            buttons += [("Load Default", self.load_per_file, 0, 3),
                         ("Process Files", self.process_files, 0, 4)]
             self.file_frame = make_frame(self.root)
             add_label(self.file_frame, 'File specific settings:')
             tabs = make_frame(self.file_frame)
             book = ttk.Notebook(tabs, width=50)
-            for df in self.datafiles:
+            for datafile in self.datafiles:
                 tab = make_frame(tabs)
                 widgets = make_frame(tab)
-                for row, var in enumerate(df.fields):
+                for row, var in enumerate(datafile.fields):
                     self.make_widget(widgets, var, row)
-                widgets.pack(fill=BOTH, expand=1)
+                widgets.pack(fill='both', expand=1)
                 save = partial(self.save_history, self.df_history,
-                               df.fields)
-                def remove_file(datafile):
-                    self.filenames.remove(datafile.in_filename.get())
-                    self.datafiles.remove(datafile)
-                    self.initialize()
-                rm = partial(remove_file, df)
+                               datafile.fields)
+
+                rm = partial(self.remove_file, datafile)
                 load = partial(self.load_history, self.df_history,
-                               df.fields)
+                               datafile.fields)
                 buttonlist = [
                     ('Save Entries', save, 0, 1),
                     ('Remove File', rm, 0, 2),
                     ('Load Default', load, 0, 3)]
                 make_buttonbox(tab, buttonlist)
-                fname = os.path.basename(df.in_filename.stringvar.get())
+                fname = os.path.basename(datafile.in_filename.stringvar.get())
                 maxlen = 7
                 if len(fname) > maxlen:
                     fname = fname[:maxlen] + '...'
                 book.add(tab, text=fname)
-            tabs.pack(fill=BOTH, expand=1)
-            book.pack(fill=BOTH, expand=1)
+            tabs.pack(fill='both', expand=1)
+            book.pack(fill='both', expand=1)
 
-            self.file_frame.grid(row=0, column=0, sticky=(N, W, E, S))
+            self.file_frame.grid(row=0, column=0, sticky=('n', 'w', 'e', 's'))
 
         make_buttonbox(self.global_frame, buttons)
         self.global_frame.update()
-        self.global_frame.grid(row=1, column=0, sticky=(N, W, E, S))
+        self.global_frame.grid(row=1, column=0, sticky=('n', 'w', 'e', 's'))
         self.root.update()
 
 
+    def remove_file(self, datafile):
+        self.filenames.remove(datafile.in_filename.get())
+        self.datafiles.remove(datafile)
+        self.initialize()
+
     def add_files(self):
+        """Add a new file tab to the file frame."""
         new_fnames = set(fileprompt(multiple=True)) - self.filenames
         self.filenames |= new_fnames
-        new_datafiles = [Datafile(fname, self.instruments)
+        new_datafiles = [Datafile(fname, __instruments__)
                          for fname in new_fnames]
         self.datafiles += new_datafiles
         if self.filenames:
@@ -119,6 +140,7 @@ class Wavegui:
 
 
     def process_files(self):
+        """Run the csv to netCDF conversion on the selected files."""
         message = ('Processing files, this may take a few minutes.')
         d = MessageDialog(self.root, message=message,
                           title='Processing...', buttons=0,
@@ -140,6 +162,7 @@ class Wavegui:
 
 
     def read_file(self, datafile, dialog):
+        """Read data from the csv file into a 'device'"""
         fields = self.global_fields + datafile.fields
         for var in fields:
             if ((var.in_air_pressure and self.air_pressure) or \
@@ -150,10 +173,10 @@ class Wavegui:
                               " please fill out all fields.",
                               title='Incomplete!', wait=True)
                 return False
-        device = self.instruments[datafile.instrument.get()]()
+        device = __instruments__[datafile.instrument.get()]()
         for var in fields:
-            if ((var.in_air_pressure and self.air_pressure) or
-                (var.in_water_pressure and not self.air_pressure)):
+            if (var.in_air_pressure and self.air_pressure) or \
+                (var.in_water_pressure and not self.air_pressure):
                 if var.name_in_device:
                     print(var.name_in_device)
                     setattr(device, var.name_in_device, var.get())
@@ -162,6 +185,7 @@ class Wavegui:
 
 
     def write_file(self, device, start_point):
+        """Write data from the 'device' to a netCDF file"""
         device.user_data_start_flag = start_point
         out_file = device.out_filename
         if os.path.isfile(out_file):
@@ -171,13 +195,19 @@ class Wavegui:
 
 
     def save_history(self, filename, varlist):
+        """Save per-file entries to a history file for later use"""
         with open(filename, 'w') as f:
             for var in varlist:
                 if var.autosave:
                     f.write(var.stringvar.get() + '\n')
 
 
+    def load_per_file(self):
+        for dfile in self.datafiles:
+            self.load_history(self.df_history, dfile.fields)
+
     def load_history(self, filename, fields):
+        """Load saved per-file entries into a file's tab"""
         any_fields_filled = (
             any(v.stringvar.get()
                 for v in self.global_fields
@@ -196,6 +226,7 @@ class Wavegui:
 
 
     def make_widget(self, frame, var, row):
+        """Make a widget based on the properties of a Variable."""
         air = self.air_pressure
         air_var = var.in_air_pressure
         water_var = var.in_water_pressure
@@ -206,11 +237,11 @@ class Wavegui:
             fname = os.path.basename(var.stringvar.get())
             add_label(frame, text=fname, pos=(row, 2))
         elif var.options:
-            OptionMenu(frame, var.stringvar, *var.options)\
-                .grid(column=2, row=row, sticky=(W, E))
+            tkinter.OptionMenu(frame, var.stringvar, *var.options)\
+                .grid(column=2, row=row, sticky=('w', 'e'))
         else:
             ttk.Entry(frame, width=20, textvariable=var.stringvar)\
-                .grid(column=2, row=row, sticky=(W, E))
+                .grid(column=2, row=row, sticky=('w', 'e'))
         if var.doc:
             c = lambda: MessageDialog(self.root, message=var.doc,
                                       title='Help')
@@ -218,12 +249,14 @@ class Wavegui:
 
 
 def make_frame(frame, header=None):
+    """Make a frame with uniform padding."""
     return ttk.Frame(frame, padding="3 3 5 5")
 
 
 def add_button(frame, text, command, row, column):
+    """Make and grid a button in a uniform way."""
     b = ttk.Button(frame, text=text, command=command)
-    b.grid(column=column, row=row, sticky=W)
+    b.grid(column=column, row=row, sticky='w')
     return b
 
 
@@ -233,22 +266,29 @@ def make_buttonbox(frame, buttonlist):
     for b in buttonlist:
         text, command, row, column = b
         add_button(box, text, command, row, column)
-    box.pack(fill=BOTH, expand=1)
+    box.pack(fill='both', expand=1)
     return box
 
 
 def add_label(frame, text, pos=None):
+    """Make a label and pack/grid it"""
     label = ttk.Label(frame, text=text)
     if pos:
         row, column = pos
-        label.grid(column=column, row=row, sticky=W)
+        label.grid(column=column, row=row, sticky='w')
     else:
-        label.pack(fill=BOTH, expand=1)
+        label.pack(fill='both', expand=1)
     return label
 
 
 class Datafile:
+    """
+    A container for Variable objects.
 
+    This class represents the data from a single pressure file. The
+    data is read in from a csv and from fields in the GUI, stored in
+    Variable objects, and can be written to a netCDF file.
+    """
     def __init__(self, filename, instruments):
         self.in_filename = Variable(name_in_device='in_filename',
                                     label='Input filename:',
@@ -334,12 +374,13 @@ class Datafile:
         self.out_filename.stringvar.set(filename + '.nc')
 
 
-class MessageDialog(Toplevel):
+class MessageDialog(tkinter.Toplevel):
+    """ A template for nice dialog boxes. """
 
     def __init__(self, parent, message="", title="", buttons=1,
                  wait=True):
-        Toplevel.__init__(self, parent)
-        body = Frame(self)
+        tkinter.Toplevel.__init__(self, parent)
+        body = ttk.Frame(self)
         self.title(title)
         self.boolean = None
         self.parent = parent
@@ -350,27 +391,35 @@ class MessageDialog(Toplevel):
             b.pack(pady=5)
         elif buttons == 2:
             buttonframe = make_frame(body)
+
             def event(boolean):
                 self.boolean = boolean
                 self.destroy()
+
             b1 = ttk.Button(buttonframe, text='YES',
-                        command=lambda: event(True))
+                            command=lambda: event(True))
             b1.grid(row=0, column=0)
             b2 = ttk.Button(buttonframe, text='NO',
-                        command=lambda: event(False))
+                            command=lambda: event(False))
             b2.grid(row=0, column=1)
             buttonframe.pack()
 
         body.pack()
         self.grab_set()
         self.geometry("+%d+%d" % (parent.winfo_rootx()+50,
-                                 parent.winfo_rooty()+50))
+                                  parent.winfo_rooty()+50))
         if wait:
             self.wait_window(self)
 
 
 class Variable:
+    """
+    Stores data about each attribute to be added to the netCDF file.
 
+    Also contains metadata that allows the GUI to build widgets from
+    the Variable and use the data inside it in the csv-to-netCDF
+    converters.
+    """
     def __init__(self, name_in_device=None, label=None,
                  doc=None, options=None, required=True,
                  filename=False, valtype=str, autosave=False,
@@ -379,7 +428,7 @@ class Variable:
         self.label = label
         self.doc = doc
         self.options = options
-        self.stringvar = StringVar()
+        self.stringvar = tkinter.StringVar()
         self.stringvar.set('')
         self.required = required
         self.filename = filename
@@ -389,11 +438,12 @@ class Variable:
         self.in_water_pressure = in_water_pressure
 
     def get(self):
+        """Cast the data in the StringVar to the right type."""
         val = self.stringvar.get()
         return self.valtype(val)
 
 
 if __name__ == '__main__':
-    root = Tk()
+    root = tkinter.Tk()
     gui = Wavegui(root, air_pressure=True)
     root.mainloop()
