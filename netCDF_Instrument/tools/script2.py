bf3e2b1e6ccb72fc9659b02611242c31bec97a33
Chris Mazzullo <chris.mazzullo@gmail.com>
2015-01-08 18:49:19 -0500

    Tested guis

--- netCDF_Instrument/tools/script2.py
+++ netCDF_Instrument/tools/script2.py
@@ -20,17 +20,26 @@ import NetCDF_Utils.nc as nc
 
 
 def make_depth_file(water_fname, air_fname, out_fname, method='fft'):
+    """Adds depth information to a water pressure file.
+
+    The argument air_fname is optional, when set to '' no air
+    pressure is used.
+    """
     device_depth = -1 * nc.get_device_depth(water_fname)
     water_depth = nc.get_water_depth(water_fname)
     timestep = 1 / nc.get_frequency(water_fname)
     sea_pressure = nc.get_pressure(water_fname)
     sea_time = nc.get_time(water_fname)
-    raw_air_pressure = nc.get_air_pressure(air_fname)
-    air_time = nc.get_time(air_fname)
 
-    air_pressure = np.interp(sea_time, air_time, raw_air_pressure,
-                             left=nc.FILL_VALUE, right=nc.FILL_VALUE)
-    corrected_pressure = sea_pressure - air_pressure
+    if air_fname != '':
+        raw_air_pressure = nc.get_air_pressure(air_fname)
+        air_time = nc.get_time(air_fname)
+        air_pressure = np.interp(sea_time, air_time, raw_air_pressure,
+                                 left=nc.FILL_VALUE, right=nc.FILL_VALUE)
+        corrected_pressure = sea_pressure - air_pressure
+        nc.append_air_pressure(out_fname, air_pressure)
+    else:
+        corrected_pressure = sea_pressure
 
     if method == 'fft':
         depth = p2d.fft_method(sea_time, corrected_pressure,
@@ -44,5 +53,4 @@ def make_depth_file(water_fname, air_fname, out_fname, method='fft'):
         raise TypeError('Accepted values for "method" are: fft, '
                         'method2 and naive.')
     shutil.copy(water_fname, out_fname)
-    nc.append_air_pressure(out_fname, air_pressure)
     nc.append_depth(out_fname, depth)
