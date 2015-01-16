290f1f5d0b675a5b057e5cae232e54dd96b8e51c
Chris Mazzullo <chris.mazzullo@gmail.com>
2015-01-15 19:04:07 -0500

    Decoupled script1 and GUI

--- netCDF_Instrument/tests/test_script1.py
+++ netCDF_Instrument/tests/test_script1.py
@@ -7,65 +7,103 @@ Coverage:
 Metadata in the netCDF produced by script1 (variables and attributes)
 Pressure data in the netCDF
 """
+# As we discussed earlier, separating GUI and script1 function will
+# help make this more testable Make sure to have modular functions for
+# each action so that they can be tested for their specific purpose
+# Such as for Script1.py such as accurate conversions, final data
+# accuracy, and other methods removed from the GUI Similarly, make
+# sure all the GUI actions are tested, (majority of buttons, not
+# necessarily actions carried out by script1 as a result) Whatever
+# method tkinter uses to listen for specific events I would test
+# through your GUI script Your methods for checking final data
+# accuracy are good, make sure to give more specific names for each
+# test even though their purposes are apparent now, e.g.
+# test_netcdf_known_sea_pressure for the last test
 
 import unittest
 from netCDF4 import Dataset
+from os.path import join
+from tools import script1
+import os
 
-csv_fname = './test_data/leveltroll_test.csv'
-test_nc_fname = './test_data/leveltroll_test.nc'
+IN_FILENAME = join('.', 'test_data', 'leveltroll_test.csv')
+OUT_FILENAME = join('.', 'test_data', 'leveltroll_test.nc')
 
-instrument_name = 'LevelTroll'
-in_filename = csv_fname,
-out_filename = test_nc_fname
-
-# all REAL metadata inputs
-# latitude = '20'
-# longitude = '30'
-# salinity = '100'
-# initial_water_depth = '11.5'
-# final_water_depth = '10'
-# device_depth = '9.5'
-# deployment_time = '20140101 0000'
-# retrieval_time = '20140102 0000'
-# tzinfo = 'US/Central'
-# sea_name = 'Great Lakes'
-# creator_name = 'Chris Mazzullo'
-# creator_email = 'stuff@gmail.com'
-# creator_url = 'zombo.com'
+INPUTS = {
+    'instrument_name' : 'LevelTroll',
+    'in_filename' : IN_FILENAME,
+    'out_filename' : OUT_FILENAME,
+    'latitude' : '20.0',
+    'longitude' : '30.0',
+    'salinity' : '100.0',
+    'initial_water_depth' : '11.5',
+    'final_water_depth' : '10.0',
+    'device_depth' : '9.5',
+    'deployment_time' : '20140101 0000',
+    'retrieval_time' : '20140102 0000',
+    'tzinfo' : 'US/Central',
+    'sea_name' : 'Great Lakes',
+    'creator_name' : 'Chris Mazzullo',
+    'creator_email' : 'stuff@gmail.com',
+    'creator_url' : 'zombo.com',
+    'sea_pressure' : True }
 
 
 class KnownValues(unittest.TestCase):
 
-    # have to add decimal point to all numbers because they are
-    # converted to floats internally
-    known_netcdf_attributes = (
-        ('deployment_time', '20140101 0000'),
-        ('retrieval_time', '20140102 0000'),
-        ('sea_name', 'Great Lakes'),
-        ('creator_name', 'Chris Mazzullo'),
-        ('creator_email', 'stuff@gmail.com'),
-        ('creator_url', 'zombo.com'),
-        ('initial_water_depth', '11.5'),
-        ('final_water_depth', '10.0'),
-        ('device_depth', '9.5'),
-        ('salinity', '100.0'))
+    def setUp(self):
+        script1.convert_to_netcdf(INPUTS)
 
 
-    def test_netcdf_known_netcdf_attributes(self):
-        with Dataset(test_nc_fname, 'r', format='NETCDF4_CLASSIC') as ds:
-            for tag, value in self.known_netcdf_attributes:
-                result = getattr(ds, tag)
-                self.assertEqual(str(value), str(result))
+    def tearDown(self):
+        if os.path.exists(OUT_FILENAME):
+            os.remove(OUT_FILENAME)
 
-    known_variables = (
-        ('longitude', '30.0'),
-        ('latitude', '20.0'))
 
-    def test_netcdf_known_variables(self):
-        with Dataset(test_nc_fname, 'r', format='NETCDF4_CLASSIC') as ds:
-            for tag, value in self.known_variables:
-                result = ds.variables[tag].getValue()
-                self.assertEqual(str(value), str(result))
+    known_numerical_netcdf_attributes = [
+        'initial_water_depth',
+        'final_water_depth',
+        'device_depth',
+        'salinity' ]
+
+
+    def test_known_numerical_netcdf_attributes(self):
+        with Dataset(OUT_FILENAME, 'r', format='NETCDF4_CLASSIC') as ds:
+            for key in self.known_numerical_netcdf_attributes:
+                known_value = INPUTS[key]
+                result = getattr(ds, key)
+                self.assertEqual(float(known_value), float(result))
+
+
+    known_string_netcdf_attributes = [
+        'deployment_time',
+        'retrieval_time',
+        'sea_name',
+        'creator_name',
+        'creator_email',
+        'creator_url' ]
+
+
+    def test_known_string_netcdf_attributes(self):
+        with Dataset(OUT_FILENAME, 'r', format='NETCDF4_CLASSIC') as ds:
+            for key in self.known_string_netcdf_attributes:
+                known_value = INPUTS[key]
+                result = getattr(ds, key)
+                self.assertEqual(str(known_value), str(result))
+
+
+    known_numerical_netcdf_variables = [
+        'longitude',
+        'latitude' ]
+
+
+    def test_known_numerical_netcdf_variables(self):
+        with Dataset(OUT_FILENAME, 'r', format='NETCDF4_CLASSIC') as ds:
+            for key in self.known_numerical_netcdf_variables:
+                known_value = INPUTS[key]
+                result = ds.variables[key].getValue()
+                self.assertEqual(str(known_value), str(result))
+
 
     # this is pulled directly from the CSV
     known_pressure_series = [ 14.735, 14.735, 14.732, 14.731, 14.729,
@@ -102,10 +140,11 @@ class KnownValues(unittest.TestCase):
                               14.719, 14.721, 14.725, 14.725, 14.726,
                               14.728, 14.728, 14.729, 14.729, 14.730,
                               14.732, 14.730, 14.731, 14.729, 14.730,
-                              14.731, 14.729]
+                              14.731, 14.729 ]
+
 
     def test_netcdf_known_variables(self):
-        with Dataset(test_nc_fname, 'r', format='NETCDF4_CLASSIC') as ds:
+        with Dataset(OUT_FILENAME, 'r', format='NETCDF4_CLASSIC') as ds:
             result_pressure_series = ds.variables['sea_water_pressure'][:]
             for known_p, result_p in zip(self.known_pressure_series,
                                          result_pressure_series):
--- netCDF_Instrument/tools/script1.py
+++ netCDF_Instrument/tools/script1.py
@@ -7,52 +7,66 @@ from Instruments.house import House
 from Instruments.measuresys import MeasureSysLogger
 from Instruments.hobo import Hobo
 
-sea_pressure = True
 
-inputs = { 'instrument_name' : 'Measurement Specialties',
-           'in_filename' : '/home/chris/work/wave-sensor/data_files/logger1.csv',
-           'out_filename' : '/home/chris/testfile.nc',
-           'latitude' : '20',
-           'longitude' : '20',
-           'salinity' : '100',
-           'initial_water_depth' : '11',
-           'final_water_depth' : '10',
-           'device_depth' : '9.5',
-           'deployment_time' : '20140101 0000',
-           'retrieval_time' : '20140102 0000',
-           'tzinfo' : 'US/Central',
-           'sea_name' : 'Atlantic Ocean',
-           'creator_name' : 'Chris Mazzullo',
-           'creator_email' : 'stuff@gmail.com',
-           'creator_url' : 'zombo.com' }
+INSTRUMENTS = {
+    'LevelTroll': Leveltroll,
+    'RBRSolo': RBRSolo,
+    'Wave Guage': Waveguage,
+    'USGS Homebrew': House,
+    'Measurement Specialties': MeasureSysLogger,
+    'HOBO': Hobo }
 
-DATATYPES = { 'latitude': np.float32,
-              'longitude': np.float32,
-              'salinity': np.float32,
-              'initial_water_depth': np.float32,
-              'final_water_depth': np.float32,
-              'device_depth': np.float32,
-              'tzinfo': timezone }
 
-for key in inputs: # cast everything to the right type
-    if key in DATATYPES:
-        inputs[key] = DATATYPES[key](inputs[key])
+def convert_to_netcdf(inputs):
+    translated = translate_inputs(inputs)
+    instrument = INSTRUMENTS[translated['instrument_name']]()
+    instrument.user_data_start_flag = 0
+    for key in translated:
+        setattr(instrument, key, translated[key])
+    instrument.read()
+    instrument.write(sea_pressure=translated['sea_pressure'])
 
-INSTRUMENTS = {'LevelTroll': Leveltroll,
-               'RBRSolo': RBRSolo,
-               'Wave Guage': Waveguage,
-               'USGS Homebrew': House,
-               'Measurement Specialties': MeasureSysLogger,
-               'HOBO': Hobo}
 
-instrument = INSTRUMENTS[inputs['instrument_name']]()
+DATATYPES = {
+    'latitude': np.float32,
+    'longitude': np.float32,
+    'salinity': np.float32,
+    'initial_water_depth': np.float32,
+    'final_water_depth': np.float32,
+    'device_depth': np.float32,
+    'tzinfo': timezone,
+    'sea_pressure' : bool }
 
-instrument.user_data_start_flag = 0
-for key in inputs:
-    setattr(instrument, key, inputs[key])
 
-instrument.read()
-instrument.write(sea_pressure=sea_pressure)
+def translate_inputs(inputs):
+    translated = dict()
+    for key in inputs: # cast everything to the right type
+        if key in DATATYPES:
+            translated[key] = DATATYPES[key](inputs[key])
+        else:
+            translated[key] = inputs[key]
+    return translated
 
-# if os.path.isfile(out_file):
-#     os.remove(out_file)
+
+if __name__ == '__main__':
+
+    inputs = {
+        'instrument_name' : 'Measurement Specialties',
+        'in_filename' : '/home/chris/work/wave-sensor/data_files/logger1.csv',
+        'out_filename' : '/home/chris/testfile.nc',
+        'latitude' : '20',
+        'longitude' : '20',
+        'salinity' : '100',
+        'initial_water_depth' : '11',
+        'final_water_depth' : '10',
+        'device_depth' : '9.5',
+        'deployment_time' : '20140101 0000',
+        'retrieval_time' : '20140102 0000',
+        'tzinfo' : 'US/Central',
+        'sea_name' : 'Atlantic Ocean',
+        'creator_name' : 'Chris Mazzullo',
+        'creator_email' : 'stuff@gmail.com',
+        'creator_url' : 'zombo.com',
+        'sea_pressure' : True }
+
+    convert_to_netcdf(inputs)
