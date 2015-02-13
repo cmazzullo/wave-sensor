44603738328fe1eb86df1207db746346d3ace8c8
Chris Mazzullo <chris.mazzullo@gmail.com>
2015-02-13 06:56:40 -0500

    Added setup.py for building

---
+++ setup.py
@@ -0,0 +1,31 @@
+"""
+Freezing script for tools in this package.
+
+To build into an executable:
+python3 setup.py build
+
+To create binary distribution:
+python3 setup.py bdist
+
+To create an executable installer for MS Windows:
+python3 setup.py bdist_wininst
+"""
+
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need
+# fine tuning.
+buildOptions = dict(packages = [], excludes = [])
+
+import sys
+base = 'Win32GUI' if sys.platform=='win32' else None
+
+executables = [
+    Executable('netCDF_Instrument/tools/compare_methods_gui.py', base=base)
+]
+
+setup(name='compare_methods',
+      version = '1.0',
+      description = 'Compare different ways of calculating water depth.',
+      options = dict(build_exe = buildOptions),
+      executables = executables)
