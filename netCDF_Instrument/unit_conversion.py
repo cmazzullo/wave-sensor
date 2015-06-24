9ea5051edbddf8fdc0c734781a8e3cb6546592df
Chris Mazzullo <chris.mazzulo@gmail.com>
2015-06-24 15:12:28 -0400

    Added USGS prototype conversion

--- netCDF_Instrument/unit_conversion.py
+++ netCDF_Instrument/unit_conversion.py
@@ -9,3 +9,5 @@ pressure_in_atm = 1.023 pressure_in_dbar = ATM_TO_DBAR * pressure_in_atm
 PSI_TO_DBAR = 0.68947573
 ATM_TO_DBAR = 10.1325
 PASCAL_TO_DBAR = 0.0001
+
+USGS_PROTOTYPE_VOLTS_TO_DBAR_FUNCTION = lambda x: 0.002527 * x + 5.9984
